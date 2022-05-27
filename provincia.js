7d51f85c7282182377397b34b1b9e6d734874dac
Ezequiel Gonzalez G <ezedrojas03@gmail.com>
2022-05-27 18:24:03 -0300

    corrijo los errores

--- provincia.js
+++ provincia.js
@@ -1,6 +1,8 @@
+const provincias = ["Salta","Córdoba","Bs.As.","Formosa"]
+
 console.log(provincias[0]);
 console.log(provincias[1]);
 console.log(provincias[2]);
 console.log(provincias[3]);
 
-const provincias = ["Salta","Córdoba","Bs.As.","Formosa"]
+
